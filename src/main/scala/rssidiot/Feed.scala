8c209d75ebfc1ff06ec36846a8fafe76cd356f16
Matthias Hansen <matthias.hansen@rwth-aachen.de>
2016-07-19 23:43:49 +0200

    Worked on Feed class

--- src/main/scala/rssidiot/Feed.scala
+++ src/main/scala/rssidiot/Feed.scala
@@ -1,9 +1,11 @@
 package rssidiot
 import rssidiot.Types._
 import rssidiot.collection.CircularBuffer
-class Feed(val url:Url,var name:String,val historySize:Int = 100) {
-   val articles = new CircularBuffer[Article](historySize)
-   def fetchNewArticles() {
-        
-   }
+class Feed(val url:Url,val name:String,val historySize:Int = 100) {
+    val articles = new CircularBuffer[Article](historySize)
+    def fetchNewArticles() {
+        val items = WebContentFetcher.fetchContentFrom(this.url)
+        val articles = items.map(Article.fromXmlItem)
+        println(articles)
+    }
 }
