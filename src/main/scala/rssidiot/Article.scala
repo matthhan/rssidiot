80a40e7c34a72c73145b6b0ee298f2b2f23272ef
Matthias Hansen <matthias.hansen@rwth-aachen.de>
2016-07-19 18:01:51 +0200

    Fixed article class after tests

--- src/main/scala/rssidiot/Article.scala
+++ src/main/scala/rssidiot/Article.scala
@@ -3,9 +3,9 @@ import rssidiot.Types._
 
 class Article(val guid:Guid,val title:Title) {
     def printableString(length:Int = 75):String = 
-        if(this.title.length > length) 
-          this.title.substring(0,length-1) 
-        else 
-          this.title
-    def equals(that:Article):Boolean = this.guid == that.guid
+        if(this.title != null) this.title.take(length) 
+        else "(null)"
+    def ==(that:Article):Boolean = 
+        if(that != null) this.guid == that.guid
+        else false
 }
