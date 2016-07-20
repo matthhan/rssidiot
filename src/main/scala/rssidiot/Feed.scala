d4983490d3e2a84d1922136872a571fb058d8925
Matthias Hansen <matthias.hansen@rwth-aachen.de>
2016-07-20 09:51:10 +0200

    implemented fetching new articles

--- src/main/scala/rssidiot/Feed.scala
+++ src/main/scala/rssidiot/Feed.scala
@@ -5,7 +5,11 @@ class Feed(val url:Url,val name:String,val historySize:Int = 100) {
     val articles = new CircularBuffer[Article](historySize)
     def fetchNewArticles() {
         val items = WebContentFetcher.fetchContentFrom(this.url)
-        val articles = items.map(Article.fromXmlItem)
-        println(articles)
+        val newArticles = items.map(Article.fromXmlItem)
+        val oldArticles = this.articles.asArray
+        newArticles.foreach(article => 
+            if (! (oldArticles contains article)) this.articles += article
+        )
     }
+    def hasNewArticles =  !(this.articles.asArray.filter(_.unread).isEmpty)
 }
