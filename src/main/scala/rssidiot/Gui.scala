110b780d9ba05a20d3d7d88b16cf743e2cd15060
Matthias Hansen <matthias.hansen@rwth-aachen.de>
2016-07-24 00:49:06 +0200

    added keyboard shortcuts to gui

--- src/main/scala/rssidiot/Gui.scala
+++ src/main/scala/rssidiot/Gui.scala
@@ -4,13 +4,13 @@ import scalafx.Includes._
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
 import scalafx.scene.Scene
-import scalafx.scene.layout.HBox
-import scalafx.scene.layout.VBox
-import scalafx.scene.layout.BorderPane
 import scalafx.scene.control.SplitPane
 import scalafx.scene.paint.Color._
-import scalafx.collections.ObservableBuffer
 import scalafx.scene.control._
+import scalafx.scene.input.KeyEvent
+import scalafx.event.EventType
+import scalafx.scene.input.KeyCode
+
 
 
 
@@ -36,6 +36,25 @@ object Gui extends JFXApp {
                 items.addAll(feedView,articleView)
                 dividerPositions = 0
             }
+        }
+        filterEvent(KeyEvent.Any) { (event:KeyEvent) => 
+            if(event.eventType == KeyEvent.KeyPressed) {
+                event.code match {
+                    //TODO: add functionality to open browser at article when
+                    //space is pressed
+                    //TODO: make sure the articleView and FeedView get into
+                    //Focus when their button is pressed
+                    //TODO make sure they scroll to ensure that the selected
+                    //Article/Feed is in view
+                    case KeyCode.Space => println("spaaace")
+                    case KeyCode.D => feedView.selectionModel().selectPrevious
+                    case KeyCode.F => feedView.selectionModel().selectNext
+                    case KeyCode.J => articleView.selectionModel().selectNext
+                    case KeyCode.K => articleView.selectionModel().selectPrevious
+                    case _ =>
+                }
+            }
+            event.consume
         }
     }
 }
