97a6f814318b5098bd64cc12d3da3b234d95f0cd
Matthias Hansen <matthias.hansen@rwth-aachen.de>
2016-07-25 00:45:29 +0200

    Added utility function to open a webbrowser and corresponding test

--- src/main/scala/rssidiot/Utility.scala
+++ src/main/scala/rssidiot/Utility.scala
@@ -1,10 +1,19 @@
 package rssidiot
 
 import java.io.PrintWriter
+import java.awt.Desktop
+import java.net.URI
+import rssidiot.Types._
 object Utility {
     def writeStringToFile(s:String,filename:String) {
         new PrintWriter(filename) {write(s);close}
     }
     def readStringFromFile(filename:String):String = 
         scala.io.Source.fromFile(filename).mkString
+    def openWebbrowserAt(url:Url) {
+        if(Desktop.isDesktopSupported)
+            Desktop.getDesktop.browse(new URI(url))
+        else 
+            println("Desktop not supported")
+    }
 }
