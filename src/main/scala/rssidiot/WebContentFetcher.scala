5622c19f1726add789aa8b02ee44e10798a92972
Matthias Hansen <matthias.hansen@rwth-aachen.de>
2016-07-25 17:49:57 +0200

    Catch an exception

--- src/main/scala/rssidiot/WebContentFetcher.scala
+++ src/main/scala/rssidiot/WebContentFetcher.scala
@@ -3,7 +3,16 @@ import rssidiot.Types._
 import scala.io.Source
 import scala.xml.XML
 object WebContentFetcher {
-    private def downloadString(url:Url):String = Source.fromURL(url).mkString
+    private def downloadString(url:Url):String = {
+        var res = ""
+        try {
+            res = Source.fromURL(url).mkString
+        } catch {
+            case e: java.net.UnknownHostException =>
+                System.err.println("Could not download from ${url}")
+        }
+        return res
+    }
     private def parseToXml(s:String) = XML.loadString(s)
     def fetchContentFrom(url:Url) = parseToXml(downloadString(url))
 }
