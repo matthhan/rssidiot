ceaa41602eec0e310c35e5643fad5e9382f9aa58
Matthias Hansen <matthias.hansen@rwth-aachen.de>
2016-07-26 17:00:38 +0200

    Added code to retry downloading pages a limited number of times

--- src/main/scala/rssidiot/WebContentFetcher.scala
+++ src/main/scala/rssidiot/WebContentFetcher.scala
@@ -4,24 +4,33 @@ import scala.io.Source
 import scala.xml.XML
 import scala.xml.Elem
 object WebContentFetcher {
-    private def downloadString(url:Url):String = {
-        var res = ""
+    private def downloadString(url:Url,numTries:Int = 10):String = {
+            for(i <- 1 to 10) {
+                try {
+                    return this.makeAttemptToDowloadString(url)
+                } catch {
+                    //Usually the case when we get HTML status code 503:
+                    //Just try again after a while
+                    case e:java.io.IOException => {
+                        Thread.sleep(500)
+                        System.err.println(e.getMessage)
+                    }
+                }
+            }
+            //Give up
+            null
+    }
+    private def makeAttemptToDowloadString(url:Url):String = {
+        System.err.println("Downloading from: " + url)
         try {
-            res = Source.fromURL(url).mkString
+            return Source.fromURL(url).mkString
         } catch {
-            //Just somehow unknown Url
+            //Just somehow unknown Url. We can really only give up in this case
             case e: java.net.UnknownHostException =>
                 System.err.println("Could not download from ${url}")
-            //Usually the case when we get HTML status code 503:
-            //Just try again after a while
-            //TODO: Limit the amount of tries so we do not get stuck in 
-            //an infinite Loop
-            case e:java.io.IOException => {
-                Thread.sleep(100)
-                return downloadString(url)
-            }
+                null
         }
-        return res
+
     }
     private def parseToXml(s:String):Elem = {
         try {
