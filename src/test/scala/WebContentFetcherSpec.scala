dec7b1717d881e164c8de21a761be8f373a0f028
Matthias Hansen <matthias.hansen@rwth-aachen.de>
2016-07-28 02:28:53 +0200

    added tests

--- src/test/scala/WebContentFetcherSpec.scala
+++ src/test/scala/WebContentFetcherSpec.scala
@@ -3,7 +3,13 @@ import rssidiot.WebContentFetcher
 
 class WebContentFetcherSpec extends FlatSpec {
     "A WebContentFetcher" should "be able to fetch items from a given URL" in {
-        val items = WebContentFetcher.fetchContentFrom("https://matthhan.github.io/rssidiot/test.rss")
-        assert(items.length != 0)
+        val fetchedString = WebContentFetcher.fetchContentFrom("https://matthhan.github.io/rssidiot/test.rss")
+        assert(fetchedString.length != 0)
+    }
+    it should "return null when handed invalid xml" in {
+        assertResult(null) {WebContentFetcher.fetchContentFrom("file:src/test/resources/invalid.xml")}
+    }
+    it should "return null when handed an unreachable url" in {
+        assertResult(null) {WebContentFetcher.fetchContentFrom("www.mis.bistutsurllurbflurgnurg")}
     }
 }
