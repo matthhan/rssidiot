429be7c933bf69690ba82f9b963a1a562cb37f6b
Matthias Hansen <matthias.hansen@rwth-aachen.de>
2016-07-28 02:29:14 +0200

    added some tests

--- src/test/scala/UtilitySpec.scala
+++ src/test/scala/UtilitySpec.scala
@@ -3,6 +3,16 @@ import rssidiot.Utility
 
 class UtilitySpec extends FlatSpec {
     "The Utility" should "be able to determine which os we are currently running" in {
-        val opSys = Utility.getOperatingSystem()
+        val opSys = Utility.getOperatingSystem
+    }
+    it should "always give us a defined folder in which we can store files" in {
+        val f = Utility.defaultDataFolder
+        assert(f != "")
+        assert(f != null)
+    }
+    it should "always give us a defined file in which to store the feeddb" in {
+        val f = Utility.defaultFeedDatabaseFile
+        assert(f != "")
+        assert(f != null)
     }
 }
