62492e818aea8847dcc9b08f4feab927f4c2b5bf
Matthias Hansen <matthias.hansen@rwth-aachen.de>
2016-08-07 22:44:52 +0200

    Made feed downloads run in parallel, which accelerates startup

--- src/main/scala/rssidiot/FeedDatabase.scala
+++ src/main/scala/rssidiot/FeedDatabase.scala
@@ -3,13 +3,16 @@ package rssidiot
 import JsonLibraryAdapter._
 
 class FeedDatabase {
-    private var feeds = List[Feed]()
+    private var feeds = Vector[Feed]()
     def add(feed:Feed) { 
-        if(! (feeds contains feed))  feeds = (feed :: feeds) 
+        if(! (feeds contains feed))  feeds = (feed +: feeds) 
     }
     def +=(that:Feed) {this add that}
-    def fetchAllNewArticles { feeds foreach (_.fetchNewArticles) }
-    def listFeeds:List[Feed] = feeds
+    //The par means convert to a parallel collection that holds references to
+    //the same objects. Then foreach is executed in parallel. This yields
+    //speedup while fetching new articles (since that is IO bound)
+    def fetchAllNewArticles { feeds.par.foreach (_.fetchNewArticles) }
+    def listFeeds:List[Feed] = feeds.toList
     def remove(f:Feed) {feeds = feeds filterNot(_ == f)}
     def saveTo(filename:String) { 
         Utility.writeStringToFile(s = this.json,filename = filename)
