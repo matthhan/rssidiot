29da53c3eaa324dcf257b048dc9ec5d362e871ed
Matthias Hansen <matthias.hansen@rwth-aachen.de>
2016-08-11 20:48:06 +0200

    Formatted some code more nicely

--- src/main/scala/rssidiot/Feed.scala
+++ src/main/scala/rssidiot/Feed.scala
@@ -93,7 +93,7 @@ object Feed {
       .filter(feedContentTypes contains _.getAttributeByName("type"))
       .map(_.getAttributeByName("href"))
       .map(discovered => if (discovered.startsWith("http")) discovered
-      else if (url.last == '/') url + discovered
-      else url + '/' + discovered)
+                         else if (url.last == '/') url + discovered
+                         else url + '/' + discovered)
   }
 }
