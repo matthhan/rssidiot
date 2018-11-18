5d5c45e1b20fb917806b7a6cecb52222a93a4818
Matthias Hansen <matthias.hansen@rwth-aachen.de>
2018-11-18 02:52:25 +0100

    remove commented code

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,8 @@ mod application;
 mod cursor;
 mod can_replace_at_index;
 mod render_state;
-mod example_state;
 mod keymapping;
 
-use example_state::construct_example_state;
 use keymapping::get_action_for;
 use render_state::render_state;
 use application::ApplicationState;
@@ -19,16 +17,10 @@ extern crate serde;
 extern crate serde_json;
 
 pub fn run() {
-    //let mut st = construct_example_state();
-    let mut st = ApplicationState::load_from_file("example_state.json".to_string());
+    let mut st = ApplicationState::load_from_file("src/example_state.json".to_string());
     let stin = io::stdin();
     let mut input = String::new();
 
-    //let art = Article::new(String::from("Hello"), String::from("World"));
-    //println!("{:?}",art);
-    //let art = art.set_read();
-    //println!("{:?}",art);
-    //println!("{}", serde_json::to_string(&st.feeds).unwrap());
     loop {
         match stin.read_line(&mut input) {
             Ok(_) => {
